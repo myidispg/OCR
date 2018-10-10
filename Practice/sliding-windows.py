2f516ad7e29faf4109cd18e4a5a69ae480ad3690
myidispg <myidispg@gmail.com>
2018-10-10 18:43:00 +0530

    Working on sliding windows and image pyramids. Need to run classifier on each window.

--- Practice/sliding-windows.py
+++ Practice/sliding-windows.py
@@ -6,7 +6,6 @@ Created on Wed Oct 10 17:30:37 2018
 @author: myidispg
 """
 
-from pyimagesearch.helpers import pyramid
 import argparse
 import time
 import cv2
@@ -32,19 +31,24 @@ img = cv2.imread('../Test Images/Wireless Drivers.png')
 (winW, winH) = (28,28)
 
 # loop over the image pyramid
-for resized in pyramid(image, scale = 1.5):
+for resized in pyramid(img, scale = 1.5):
     # loop over each resized window of the image pyramid
-    for (x,y, window) in sliding_window(resized, stepSize = 4, windowsSize = (winW, winH)):
+    for (x,y, window) in sliding_window(resized, stepSize = 4, windowSize = (winW, winH)):
         if window.shape[0] != winH or window.shape[1] != winW:
+            print('x- ' + str(x))
+            print('y- ' + str(y))
+            print('window' + str(window))
             continue
- 
+            
 		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
 		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
 		# WINDOW
  
 		# since we do not have a classifier, we'll just draw the window
         clone = resized.copy()
-        cv2.rectange;(clone, (x,y), (x + winW, y + winH), (0, 255, 0), 2)
+        cv2.rectangle(clone, (x,y), (x + winW, y + winH), (0, 255, 0), 2)
         cv2.imshow("Window", clone)
         cv2.waitKey(1)
         time.sleep(0.025)
+        
+# Now we can work on saving each windows and running detection on it. 
