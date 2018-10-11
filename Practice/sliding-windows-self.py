f374b3c59b844875a78be039fdca639bd003e0b6
myidispg <myidispg@gmail.com>
2018-10-11 17:44:32 +0530

    Working on own sliding windows implementation.

--- Practice/sliding-windows-self.py
+++ Practice/sliding-windows-self.py
@@ -9,9 +9,19 @@ Created on Thu Oct 11 17:31:18 2018
 import cv2
 
 def img_pyramid(image, scale):
-    pass
-
-img = cv2.imread('../Test Images/Wireless Drivers.png')
-
+    image = cv2.resize(image, (int(img.shape[0]/scale), int(img.shape[1]/scale)))
     
+    return image
+
+img = cv2.imread('../Test Images/Wireless Drivers.png', 0)
+
+# Convert the image to the nearest multiple of windows size i.e. 28
+(imgH, imgW) = (img.shape[0] - (img.shape[0]%28), img.shape[1] - img.shape[1]%28)
+
+img = cv2.resize(img, (imgW, imgH))
+# Image dimensions are rowsxcolumns
+
+(winW, winH) = (28,28)
+
+img = img_pyramid(img, 1.5)
     
