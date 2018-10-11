e68de82052da0e180d17cf459d53d9ce03a57280
myidispg <myidispg@gmail.com>
2018-10-11 17:34:59 +0530

    Removed unnecessary scripts

---
+++ Practice/sliding-windows-self.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 17:31:18 2018
+
+@author: myidispg
+"""
+
+import cv2
+
+def img_pyramid(image, scale):
+    pass
+
+img = cv2.imread('../Test Images/Wireless Drivers.png')
+
+    
+    
