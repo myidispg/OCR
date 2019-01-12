9be29784e64f944e406cd86762d9d008faf0dc62
myidispg <myidispg@gmail.com>
2019-01-12 18:42:35 +0530

    Updated gitignore

--- mnist.py
+++ mnist.py
@@ -10,14 +10,20 @@ from mnist import MNIST
 import gzip
 import numpy as np
 
-f = gzip.open('../MNIST_Dataset/train-images-idx3-ubyte.gz', 'r')
-
 image_size = 28
 num_images = 60000
 
+# Open the images
+f = gzip.open('../MNIST_Dataset/train-images-idx3-ubyte.gz', 'r')
 buf = f.read(image_size * image_size * num_images)
 data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
 data = data.reshape(num_images, image_size, image_size, 1)
+
+# Open the labels
+f = gzip.open('../MNIST_Dataset/train-labels-idx1-ubyte.gz', 'r')
+buf = f.read(32 * num_images * 8)
+labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+
 
 
 #mndata = MNIST('../MNIST_Dataset/')
@@ -30,7 +36,7 @@ data = data.reshape(num_images, image_size, image_size, 1)
 from sklearn.preprocessing import OneHotEncoder
 
 import cv2
-image = np.reshape(images[0], (28,28))
+image = np.reshape(data[0], (28,28))
 cv2.imshow('image', data[0])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
